f8d53a9069cdc429b60cbe9603ea9ae86eb33b4a
Daniel-Shliakhetko <danielshliakhetko@gmail.com>
2023-03-29 20:29:47 +0300

    Add basic routing and install sass

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
+const isAuth = true;
+
 function App() {
   return (
     <div className="App">
+      <Router>
+          {
+            isAuth ? (
+              <Routes>
+                <Route path={'/workspace'} element={"Workspace"}/>
+                <Route path={'/account'} element={"Account"}/>
+                <Route path={'*'} element={<Navigate to={'/workspace'}/>}/>
+              </Routes>
+            ) : (
+              <Routes>
+                <Route path={'/auth'} element={"Register"}/>
+                <Route path={'/'} element={"Main"}/>
+                <Route path={'*'} element={<Navigate to={'/'}/>}/>
+              </Routes>
+            )
+          }
+      </Router>
     </div>
   );
 }
